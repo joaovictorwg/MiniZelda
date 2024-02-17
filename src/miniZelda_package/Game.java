f84682c8cd53609eb0e711a72e3dba179d5d2500
João Victor Walcacer <joaovictorgiani@gmail.com>
2024-02-17 11:45:00 -0300

    renderização adicionada

--- src/miniZelda_package/Game.java
+++ src/miniZelda_package/Game.java
@@ -1,16 +1,43 @@
 package miniZelda_package;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.nio.Buffer;
 
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable{
 
-	public static int WIDTH = 1024, HEIGHT = 768;
+	public static int WIDTH = 480, HEIGHT = 480;
 
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		g.setColor(Color.red);
+		g.fillRect(0, 0, 50, 50);
+		
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
@@ -25,11 +52,22 @@ public class Game extends Canvas implements Runnable{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		frame.setVisible(true);
+		
+		new Thread(game).start();
 	}
 	
 	@Override
 	public void run() {
 		
+		while(true) {
+			tick();
+			render();
+			try {
+				Thread.sleep(1000/60);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
